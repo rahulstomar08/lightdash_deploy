fbe945569463cfe9dc1899202ad06340bbd2ab56
r7535874023@gmail.com <r7535874023@gmail.com>
2022-08-03 11:45:25 +0000

    hh

--- models/claims.sql
+++ models/claims.sql
@@ -1,9 +1,17 @@
 with intimation_date_by_LOB as (
 
+
+
     select
+
      POLICY_NO as intimated_claims_count,
+
      UPPER(PAR_NONPAR_ULIP_LEVELINCREASING) as LOB,
+
      DATE_OF_INTIMATION_HO
+
      FROM {{ source("DB_01","CLAIMSDEC")}}
+
 )
+
 SELECT * from intimation_date_by_LOB
