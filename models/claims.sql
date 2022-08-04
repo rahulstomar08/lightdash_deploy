560a0693e2e22e6d1f7a4b5a59009801e5cd800e
r7535874023@gmail.com <r7535874023@gmail.com>
2022-08-04 10:57:56 +0000

    l

--- models/claims.sql
+++ models/claims.sql
@@ -1,14 +1,18 @@
-select 
+SELECT POLICY_NO,
 
-    POLICY_NO,
     UPPER(PAR_NONPAR_ULIP_LEVELINCREASING) as LOB,
+
     DATE_OF_INTIMATION_HO,
+
     (CASE WHEN EXTRACT(MONTH from to_date(DATE_OF_INTIMATION_HO,'dd-mm-yyyy'))<4
+
     THEN CONCAT('FY', cast(EXTRACT(YEAR from to_date(DATE_OF_INTIMATION_HO,'dd-mm-yyyy')) as TEXT))
+
     WHEN EXTRACT(MONTH from to_date(DATE_OF_INTIMATION_HO,'dd-mm-yyyy'))>4
+
     THEN CONCAT('FY', cast(EXTRACT(YEAR from to_date(DATE_OF_INTIMATION_HO,'dd-mm-yyyy'))+1 as TEXT)) END )AS Fiscal_year,
+
     EXTRACT(MONTH from to_date(DATE_OF_INTIMATION_HO,'dd-mm-yyyy')) as month_ho,
     SNO,
     CLAIM_AMOUNT
-
 FROM {{ source("DB_01","CLAIMSDEC")}}
