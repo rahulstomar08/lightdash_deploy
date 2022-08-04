b62c9dbf4e156a1e1b46d5d22d4c7251b4d5dd73
r7535874023@gmail.com <r7535874023@gmail.com>
2022-08-04 09:54:08 +0000

    h

--- models/claims.sql
+++ models/claims.sql
@@ -1,11 +1,20 @@
 select 
+
     POLICY_NO,
+
     UPPER(PAR_NONPAR_ULIP_LEVELINCREASING) as LOB,
+
     DATE_OF_INTIMATION_HO,
+
     (CASE WHEN EXTRACT(MONTH from to_date(DATE_OF_INTIMATION_HO,'dd-mm-yyyy'))<4
+
     THEN CONCAT('FY', cast(EXTRACT(YEAR from to_date(DATE_OF_INTIMATION_HO,'dd-mm-yyyy')) as TEXT))
+
     WHEN EXTRACT(MONTH from to_date(DATE_OF_INTIMATION_HO,'dd-mm-yyyy'))>4
+
     THEN CONCAT('FY', cast(EXTRACT(YEAR from to_date(DATE_OF_INTIMATION_HO,'dd-mm-yyyy'))+1 as TEXT)) END )AS Fiscal_year,
+
     EXTRACT(MONTH from to_date(DATE_OF_INTIMATION_HO,'dd-mm-yyyy')) as month_ho,
-    CLAIM_AMOUNT
+    SNO
+
 FROM {{ source("DB_01","CLAIMSDEC")}}
--- models/fiscalyear.sql
+++
@@ -1,5 +0,0 @@
-SELECT (CASE WHEN EXTRACT(MONTH from to_date(DATE_OF_INTIMATION_HO,'dd-mm-yyyy'))<4
-THEN CONCAT('FY', cast(EXTRACT(YEAR from to_date(DATE_OF_INTIMATION_HO,'dd-mm-yyyy')) as TEXT))
-WHEN EXTRACT(MONTH from to_date(DATE_OF_INTIMATION_HO,'dd-mm-yyyy'))>4
-THEN CONCAT('FY', cast(EXTRACT(YEAR from to_date(DATE_OF_INTIMATION_HO,'dd-mm-yyyy'))+1 as TEXT)) END )AS Fiscal_year
-FROM {{ source("DB_01","CLAIMSDEC")}}
