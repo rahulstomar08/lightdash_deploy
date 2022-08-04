521aa128a3a8b7cb1deab23981b10b0922c665ba
r7535874023@gmail.com <r7535874023@gmail.com>
2022-08-04 06:41:15 +0000

    f

--- models/claims.sql
+++ models/claims.sql
@@ -6,5 +6,6 @@ select
     THEN CONCAT('FY', cast(EXTRACT(YEAR from to_date(DATE_OF_INTIMATION_HO,'dd-mm-yyyy')) as TEXT))
     WHEN EXTRACT(MONTH from to_date(DATE_OF_INTIMATION_HO,'dd-mm-yyyy'))>4
     THEN CONCAT('FY', cast(EXTRACT(YEAR from to_date(DATE_OF_INTIMATION_HO,'dd-mm-yyyy'))+1 as TEXT)) END )AS Fiscal_year,
-    EXTRACT(MONTH from to_date(DATE_OF_INTIMATION_HO,'dd-mm-yyyy')) as month_ho
+    EXTRACT(MONTH from to_date(DATE_OF_INTIMATION_HO,'dd-mm-yyyy')) as month_ho,
+    CLAIM_AMOUNT
 FROM {{ source("DB_01","CLAIMSDEC")}}
