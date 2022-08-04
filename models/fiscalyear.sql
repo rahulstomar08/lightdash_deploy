ad721667b3a2fb959d3de8f029f4d113abd1e5a5
r7535874023@gmail.com <r7535874023@gmail.com>
2022-08-04 06:10:36 +0000

    e

--- models/fiscalyear.sql
+++ models/fiscalyear.sql
@@ -1,8 +1,4 @@
-select 
-    POLICY_NO,
-    UPPER(PAR_NONPAR_ULIP_LEVELINCREASING) as LOB,
-    DATE_OF_INTIMATION_HO,
-    (CASE WHEN EXTRACT(MONTH from to_date(DATE_OF_INTIMATION_HO,'dd-mm-yyyy'))<4
+SELECT (CASE WHEN EXTRACT(MONTH from to_date(DATE_OF_INTIMATION_HO,'dd-mm-yyyy'))<4
 THEN CONCAT('FY', cast(EXTRACT(YEAR from to_date(DATE_OF_INTIMATION_HO,'dd-mm-yyyy')) as TEXT))
 WHEN EXTRACT(MONTH from to_date(DATE_OF_INTIMATION_HO,'dd-mm-yyyy'))>4
 THEN CONCAT('FY', cast(EXTRACT(YEAR from to_date(DATE_OF_INTIMATION_HO,'dd-mm-yyyy'))+1 as TEXT)) END )AS Fiscal_year 
