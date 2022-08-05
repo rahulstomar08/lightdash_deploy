86e1629f8a295f6d2e6f031d92c109090f2c09ee
r7535874023@gmail.com <r7535874023@gmail.com>
2022-08-05 10:27:24 +0000

    s

--- models/sum_amt.sql
+++ models/sum_amt.sql
@@ -1,9 +1,18 @@
 SELECT 
      month_ho,
-     ROUND(SUM(CLAIM_AMOUNT/10000000),1) AS CLAIM_AMOUNTS,
-     month_name,
-     Fiscal_year
+     ROUND(SUM(CLAIM_AMOUNT/10000000),1) AS CLAIM_AMOUNTS
 FROM {{ source("DB_01","claims")}}
 WHERE CLAIM_AMOUNT NOT LIKE '%-%'
-GROUP BY month_ho,month_name,Fiscal_year
-ORDER BY month_ho
+GROUP BY month_ho
+ORDER BY (case when month_ho = 1 then 1
+               when month_ho = 2 then 2
+               when month_ho = 3 then 3
+               when month_ho = 4 then 4
+               when month_ho = 5 then 5
+               when month_ho = 6 then 6
+               when month_ho = 7 then 7
+               when month_ho = 8 then 8
+               when month_ho = 9 then 9
+               when month_ho = 10 then 10
+               when month_ho = 11 then 11
+               when month_ho = 12 then 12 end )
