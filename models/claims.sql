39cb16d69781adc68e8645860a90be71ff35134c
r7535874023@gmail.com <r7535874023@gmail.com>
2022-08-08 18:16:09 +0000

    2

--- models/claims.sql
+++ models/claims.sql
@@ -4,10 +4,10 @@ select
     UPPER(PAR_NONPAR_ULIP_LEVELINCREASING) as LOB,
     TRY_TO_DATE(DATE_OF_INTIMATION_HO::VARCHAR,'dd-mm-yyyy') as Intimation_Date,
     TRY_TO_DATE(REPUDIATION_DATE_SETTLEMENT_DATE::VARCHAR,'dd-mm-yyyy') as Settlement_Date,
-    (CASE WHEN EXTRACT(MONTH from to_date(REPUDIATION_DATE_SETTLEMENT_DATE,'dd-mm-yyyy'))<4
-    THEN CONCAT('FY', cast(EXTRACT(YEAR from to_date(REPUDIATION_DATE_SETTLEMENT_DATE,'dd-mm-yyyy')) as TEXT))
-    WHEN EXTRACT(MONTH from to_date(REPUDIATION_DATE_SETTLEMENT_DATE,'dd-mm-yyyy'))>=4
-    THEN CONCAT('FY', cast(EXTRACT(YEAR from to_date(REPUDIATION_DATE_SETTLEMENT_DATE,'dd-mm-yyyy'))+1 as TEXT)) END )AS Fiscal_year,
+    (CASE WHEN EXTRACT(MONTH from to_date(DATE_OF_INTIMATION_HO,'dd-mm-yyyy'))<4
+    THEN CONCAT('FY', cast(EXTRACT(YEAR from to_date(DATE_OF_INTIMATION_HO,'dd-mm-yyyy')) as TEXT))
+    WHEN EXTRACT(MONTH from to_date(DATE_OF_INTIMATION_HO,'dd-mm-yyyy'))>=4
+    THEN CONCAT('FY', cast(EXTRACT(YEAR from to_date(DATE_OF_INTIMATION_HO,'dd-mm-yyyy'))+1 as TEXT)) END )AS Fiscal_year,
     EXTRACT(MONTH from to_date(DATE_OF_INTIMATION_HO,'dd-mm-yyyy')) as month_ho,
     EXTRACT(MONTH from to_date(REPUDIATION_DATE_SETTLEMENT_DATE,'dd-mm-yyyy')) as month_sd,
     (CASE WHEN month_ho = 1 then 'JAN'
