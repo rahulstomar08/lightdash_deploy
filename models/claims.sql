3e17e1a8e7ad1be18d78eb58c210956f1c7737b0
r7535874023@gmail.com <r7535874023@gmail.com>
2022-08-08 12:01:55 +0000

    21

--- models/claims.sql
+++ models/claims.sql
@@ -3,11 +3,13 @@ select
     POLICY_NO,
     UPPER(PAR_NONPAR_ULIP_LEVELINCREASING) as LOB,
     DATE_OF_INTIMATION_HO,
+    REPUDIATION_DATE_SETTLEMENT_DATE,
     (CASE WHEN EXTRACT(MONTH from to_date(DATE_OF_INTIMATION_HO,'dd-mm-yyyy'))<4
     THEN CONCAT('FY', cast(EXTRACT(YEAR from to_date(DATE_OF_INTIMATION_HO,'dd-mm-yyyy')) as TEXT))
     WHEN EXTRACT(MONTH from to_date(DATE_OF_INTIMATION_HO,'dd-mm-yyyy'))>4
     THEN CONCAT('FY', cast(EXTRACT(YEAR from to_date(DATE_OF_INTIMATION_HO,'dd-mm-yyyy'))+1 as TEXT)) END )AS Fiscal_year,
     EXTRACT(MONTH from to_date(DATE_OF_INTIMATION_HO,'dd-mm-yyyy')) as month_ho,
+    EXTRACT(MONTH from to_date(REPUDIATION_DATE_SETTLEMENT_DATE,'dd-mm-yyyy')) as month_sd,
     (CASE WHEN month_ho = 1 then 'JAN'
         WHEN month_ho = 2 then 'FEb'
         WHEN month_ho = 3 then 'MAR'
@@ -21,6 +23,19 @@ select
         WHEN month_ho = 11 then 'NOV'
         WHEN month_ho = 12 then 'DEC'
         end) as month_name,
+        (CASE WHEN month_sd = 1 then 'JAN'
+        WHEN month_sd = 2 then 'FEb'
+        WHEN month_sd = 3 then 'MAR'
+        WHEN month_sd = 4 then 'APR'
+        WHEN month_sd = 5 then 'MAY'
+        WHEN month_sd = 6 then 'JUNE'
+        WHEN month_sd = 7 then 'JULY'
+        WHEN month_sd = 8 then 'AUG'
+        WHEN month_sd = 9 then 'SEP'
+        WHEN month_sd = 10 then 'OCT'
+        WHEN month_sd = 11 then 'NOV'
+        WHEN month_sd = 12 then 'DEC'
+        end) as month_name_sd,
         SNO,
         CLAIM_AMOUNT
         
