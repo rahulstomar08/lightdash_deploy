3eaaf434250ee8e8219309ea6fd7806c46638084
r7535874023@gmail.com <r7535874023@gmail.com>
2022-08-08 17:27:29 +0000

    26

--- models/claims.sql
+++ models/claims.sql
@@ -4,7 +4,7 @@ select
     UPPER(PAR_NONPAR_ULIP_LEVELINCREASING) as LOB,
     TRY_TO_DATE(DATE_OF_INTIMATION_HO::VARCHAR,'dd-mm-yyyy') as Intimation_Date,
     REPUDIATION_DATE_SETTLEMENT_DATE,
-    (CASE WHEN EXTRACT(MONTH from to_date(DATE_OF_INTIMATION_HO,'dd-mm-yyyy'))<4
+    (CASE WHEN EXTRACT(MONTH from to_date(DATE_OF_INTIMATION_HO,'dd-mm-yyyy'))<=4
     THEN CONCAT('FY', cast(EXTRACT(YEAR from to_date(DATE_OF_INTIMATION_HO,'dd-mm-yyyy')) as TEXT))
     WHEN EXTRACT(MONTH from to_date(DATE_OF_INTIMATION_HO,'dd-mm-yyyy'))>4
     THEN CONCAT('FY', cast(EXTRACT(YEAR from to_date(DATE_OF_INTIMATION_HO,'dd-mm-yyyy'))+1 as TEXT)) END )AS Fiscal_year,
