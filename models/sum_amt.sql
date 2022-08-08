2fa399bcba497cb98e341e431185d87522d0b319
r7535874023@gmail.com <r7535874023@gmail.com>
2022-08-08 10:23:36 +0000

    17

--- models/sum_amt.sql
+++ models/sum_amt.sql
@@ -4,15 +4,15 @@ SELECT
 FROM {{ source("DB_01","claims")}}
 WHERE CLAIM_AMOUNT NOT LIKE '%-%'
 GROUP BY month_ho,month_name,Fiscal_year
-ORDER BY (case when month_ho = 1 then 1
-               when month_ho = 2 then 2
-               when month_ho = 3 then 3
-               when month_ho = 4 then 4
-               when month_ho = 5 then 5
-               when month_ho = 6 then 6
-               when month_ho = 7 then 7
-               when month_ho = 8 then 8
-               when month_ho = 9 then 9
-               when month_ho = 10 then 10
-               when month_ho = 11 then 11
-               when month_ho = 12 then 12 end )
+ORDER BY (CASE WHEN month_ho = 1 then 'JAN'
+        WHEN month_ho = 2 then 'FEb'
+        WHEN month_ho = 3 then 'MAR'
+        WHEN month_ho = 4 then 'APR'
+        WHEN month_ho = 5 then 'MAY'
+        WHEN month_ho = 6 then 'JUNE'
+        WHEN month_ho = 7 then 'JULY'
+        WHEN month_ho = 8 then 'AUG'
+        WHEN month_ho = 9 then 'SEP'
+        WHEN month_ho = 10 then 'OCT'
+        WHEN month_ho = 11 then 'NOV'
+        WHEN month_ho = 12 then 'DEC') end
