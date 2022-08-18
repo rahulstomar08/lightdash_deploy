64101a1f80820405d6080d2a95718c3802feea82
r7535874023@gmail.com <r7535874023@gmail.com>
2022-08-18 13:00:56 +0000

    18

--- models/claims1.sql
+++ models/claims1.sql
@@ -1,6 +1,7 @@
 select 
 
     POLICY_NO,
-    TRY_TO_DATE(REPUDIATION_DATE_SETTLEMENT_DATE::VARCHAR,'dd-mm-yyyy') as Settlement_Date
+    TRY_TO_DATE(REPUDIATION_DATE_SETTLEMENT_DATE::VARCHAR,'dd-mm-yyyy') as Settlement_Date,
+    CLAIM_AMOUNT
     
 FROM {{ source("DB_01","CLAIMSDEC")}}
