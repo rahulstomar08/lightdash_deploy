361e3b87625153f9b487a90e6e1c2309d10d1f37
r7535874023@gmail.com <r7535874023@gmail.com>
2022-08-18 13:16:45 +0000

    21

--- models/claims1.sql
+++ models/claims1.sql
@@ -2,7 +2,11 @@ select
 
     POLICY_NO,
     TRY_TO_DATE(REPUDIATION_DATE_SETTLEMENT_DATE::VARCHAR,'dd-mm-yyyy') as Settlement_Date,
-    CLAIM_AMOUNT
+    CLAIM_AMOUNT,
+    (CASE WHEN EXTRACT(MONTH from to_date(REPUDIATION_DATE_SETTLEMENT_DATE,'dd-mm-yyyy'))<4
+    THEN CONCAT('FY', cast(EXTRACT(YEAR from to_date(REPUDIATION_DATE_SETTLEMENT_DATE,'dd-mm-yyyy')) as TEXT))
+    WHEN EXTRACT(MONTH from to_date(REPUDIATION_DATE_SETTLEMENT_DATE,'dd-mm-yyyy'))>=4
+    THEN CONCAT('FY', cast(EXTRACT(YEAR from to_date(REPUDIATION_DATE_SETTLEMENT_DATE,'dd-mm-yyyy'))+1 as TEXT)) END )AS Fiscal_year
     
 FROM {{ source("DB_01","CLAIMSDEC")}}
 WHERE CLAIM_AMOUNT NOT LIKE '%-%'
