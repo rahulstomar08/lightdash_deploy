8323446a92345ff03d6c7364dc9521b21510fb0b
r7535874023@gmail.com <r7535874023@gmail.com>
2022-08-22 18:15:17 +0000

    5

--- models/claims.sql
+++ models/claims.sql
@@ -4,6 +4,7 @@ select
     UPPER(PAR_NONPAR_ULIP_LEVELINCREASING) as LOB,
     TRY_TO_DATE(DATE_OF_INTIMATION_HO::VARCHAR,'dd-mm-yyyy') as Intimation_Date,
     date(dateadd(year,-1,Intimation_Date)) as Previous_Year_same_month,
+    LAG (CLAIM_AMOUNT,1) OVER (ORDER BY Intimation_Date) AS A,
     TRY_TO_DATE(REPUDIATION_DATE_SETTLEMENT_DATE::VARCHAR,'dd-mm-yyyy') as Settlement_Date,
     (CASE WHEN EXTRACT(MONTH from to_date(DATE_OF_INTIMATION_HO,'dd-mm-yyyy'))<4
     THEN CONCAT('FY', cast(EXTRACT(YEAR from to_date(DATE_OF_INTIMATION_HO,'dd-mm-yyyy')) as TEXT))
