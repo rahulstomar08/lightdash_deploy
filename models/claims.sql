1aaff80736ea2618e520663421476197acde2df0
r7535874023@gmail.com <r7535874023@gmail.com>
2022-08-24 09:11:13 +0000

    2

--- models/claims.sql
+++ models/claims.sql
@@ -9,8 +9,6 @@ select
     THEN CONCAT('FY', cast(EXTRACT(YEAR from to_date(DATE_OF_INTIMATION_HO,'dd-mm-yyyy')) as TEXT))
     WHEN EXTRACT(MONTH from to_date(DATE_OF_INTIMATION_HO,'dd-mm-yyyy'))>=4
     THEN CONCAT('FY', cast(EXTRACT(YEAR from to_date(DATE_OF_INTIMATION_HO,'dd-mm-yyyy'))+1 as TEXT)) END )AS Fiscal_year,
-    EXTRACT(MONTH from to_date(DATE_OF_INTIMATION_HO,'dd-mm-yyyy')) as month_ho,
-    EXTRACT(MONTH from to_date(REPUDIATION_DATE_SETTLEMENT_DATE,'dd-mm-yyyy')) as month_sd,
     SNO,
     CLAIM_AMOUNT,
     SERVICING_BRANCH_NAME,
