999ee14b25b9694db854c0d6aab67f454c90a128
r7535874023@gmail.com <r7535874023@gmail.com>
2022-08-25 05:33:44 +0000

    1

--- models/claims.sql
+++ models/claims.sql
@@ -1,6 +1,5 @@
-select 
-
-    POLICY_NO,
+with final as ( 
+    SELECT
     UPPER(PAR_NONPAR_ULIP_LEVELINCREASING) as LOB,
     TRY_TO_DATE(DATE_OF_INTIMATION_HO::VARCHAR,'dd-mm-yyyy') as Intimation_Date,
     date(dateadd(year,-1,Intimation_Date)) as Previous_Year_same_month,
@@ -17,9 +16,27 @@ select
     WRITING_AGENT_STATE_NAME,
     NATURE_OF_BASE_POLICY
     
-FROM {{ source("DB_01","CLAIMSDEC")}}
-WHERE CLAIM_AMOUNT NOT LIKE '%-%'
-order by Intimation_Date
+    FROM {{ source("DB_01","CLAIMSDEC")}}
+    WHERE CLAIM_AMOUNT NOT LIKE '%-%'
+    GROUP BY LOB,Intimation_Date,Previous_Year_same_month,Settlement_Date,Fiscal_year, SNO,
+    CLAIM_AMOUNT,
+    SERVICING_BRANCH_NAME,
+    CHANNEL1,
+    EARLYNON_EARLY,
+    WRITING_AGENT_STATE_NAME,
+    NATURE_OF_BASE_POLICY
+)
+
+select *,
+LAG (SUM(CLAIM_AMOUNT),1) OVER (ORDER BY YEAR(Intimation_Date)) AS Claim_Last_Year
+FROM final
+GROUP BY LOB,Intimation_Date,Previous_Year_same_month,Settlement_Date,Fiscal_year, SNO,
+    CLAIM_AMOUNT,
+    SERVICING_BRANCH_NAME,
+    CHANNEL1,
+    EARLYNON_EARLY,
+    WRITING_AGENT_STATE_NAME,
+    NATURE_OF_BASE_POLICY
 
 
 
