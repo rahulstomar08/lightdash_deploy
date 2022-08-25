b875f5fc60a6b1c177c7568330685fa39d60affb
r7535874023@gmail.com <r7535874023@gmail.com>
2022-08-25 18:47:57 +0000

    6

--- models/claims.sql
+++ models/claims.sql
@@ -3,7 +3,6 @@ with final as (
     POLICY_NO,
     UPPER(PAR_NONPAR_ULIP_LEVELINCREASING) as LOB,
     TRY_TO_DATE(DATE_OF_INTIMATION_HO::VARCHAR,'dd-mm-yyyy') as Intimation_Date,
-    date(dateadd(year,-1,Intimation_Date)) as Previous_Year_same_month,
     TRY_TO_DATE(REPUDIATION_DATE_SETTLEMENT_DATE::VARCHAR,'dd-mm-yyyy') as Settlement_Date,
     (CASE WHEN EXTRACT(MONTH from to_date(DATE_OF_INTIMATION_HO,'dd-mm-yyyy'))<4
     THEN CONCAT('FY', cast(EXTRACT(YEAR from to_date(DATE_OF_INTIMATION_HO,'dd-mm-yyyy')) as TEXT))
@@ -23,9 +22,9 @@ with final as (
 )
 
 select *,
-LAG (SUM(CLAIM_AMOUNT)/10000000) OVER (ORDER BY YEAR(Intimation_Date)) AS Claim_Last_Year
+LAG (SUM(CLAIM_AMOUNT)/10000000) OVER (ORDER BY Fiscal_year) AS Claim_Last_Year
 FROM final
-GROUP BY POLICY_NO,LOB,Intimation_Date,Previous_Year_same_month,Settlement_Date,Fiscal_year, SNO,
+GROUP BY POLICY_NO,LOB,Intimation_Date,Settlement_Date,Fiscal_year, SNO,
     CLAIM_AMOUNT,
     SERVICING_BRANCH_NAME,
     CHANNEL1,
