8f1dd107172d01439bfc4219c01dc22b3b85fb18
r7535874023@gmail.com <r7535874023@gmail.com>
2022-08-25 18:32:02 +0000

    1

--- models/intimation_dt_view.sql
+++
@@ -1,4 +0,0 @@
-select INTIMATION_DATE ,
-        sum(CLAIM_AMOUNT) as sum_amt_claim
-       FROM {{ source("DB_01","claims")}} 
-       group by INTIMATION_DATE 
--- models/prev_intimation_dt_view.sql
+++
@@ -1,5 +0,0 @@
-select pre_inti_dt,
-        sum(CLAIM_AMOUNT) as sum_amt_pre_claim from
-(select  DATEADD(year, -1, INTIMATION_DATE) AS pre_inti_dt,
-        CLAIM_AMOUNT
-        FROM {{ source("DB_01","claims")}} ) group by pre_inti_dt
