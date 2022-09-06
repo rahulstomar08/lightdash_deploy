4032903af23830e8ab921aee62f3030b4a3b413b
r7535874023@gmail.com <r7535874023@gmail.com>
2022-09-06 04:28:42 +0000

    2

--- models/Settlement.sql
+++ models/Settlement.sql
@@ -1,15 +1,15 @@
 select 
 
-    "Policy No",
+    "Policy No" AS POLICY_NO,
     TRY_TO_DATE("Repudiation Date/ Settlement Date"::VARCHAR,'dd-mm-yyyy') as Settlement_Date,
     "Claim Amount  (BASE + RIDER)",
     (CASE WHEN EXTRACT(MONTH from to_date("Repudiation Date/ Settlement Date",'dd-mm-yyyy'))<4
     THEN CONCAT('FY', cast(EXTRACT(YEAR from to_date("Repudiation Date/ Settlement Date",'dd-mm-yyyy')) as TEXT))
     WHEN EXTRACT(MONTH from to_date("Repudiation Date/ Settlement Date",'dd-mm-yyyy'))>=4
     THEN CONCAT('FY', cast(EXTRACT(YEAR from to_date("Repudiation Date/ Settlement Date",'dd-mm-yyyy'))+1 as TEXT)) END )AS Fiscal_year,
-    "Channel-1",
-    "Early/Non Early",
-    "Writing Agent State Name",
+    "Channel-1" AS CHANNEL,
+    "Early/Non Early" AS EARLY_NON_EARLY,
+    "Writing Agent State Name" AS WRITING_AGENT_STATE_NAME,
     UPPER("PAR / Non-Par / ULIP - Level/Increasing") as LOB
     
 FROM {{ source("DB_01","CLAIMSDEC")}}
--- models/claims.sql
+++ models/claims.sql
@@ -1,6 +1,12 @@
 SELECT
     "Policy No",
     UPPER("PAR / Non-Par / ULIP - Level/Increasing") as LOB,
+    TRY_TO_DATE("Date of Intimation (HO)"::VARCHAR,'dd-mm-yyyy') as Intimation_Date,
+    TRY_TO_DATE("Repudiation Date/ Settlement Date"::VARCHAR,'dd-mm-yyyy') as Settlement_Date,
+    (CASE WHEN EXTRACT(MONTH from to_date("Date of Intimation (HO)",'dd-mm-yyyy'))<4
+    THEN CONCAT('FY', cast(EXTRACT(YEAR from to_date("Date of Intimation (HO)",'dd-mm-yyyy')) as TEXT))
+    WHEN EXTRACT(MONTH from to_date("Date of Intimation (HO)",'dd-mm-yyyy'))>=4
+    THEN CONCAT('FY', cast(EXTRACT(YEAR from to_date("Date of Intimation (HO)",'dd-mm-yyyy'))+1 as TEXT)) END )AS Fiscal_year,
     "S.No",
     "Claim Amount  (BASE + RIDER)",
     "Servicing Branch Name",
